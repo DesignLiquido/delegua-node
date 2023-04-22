3297fdb35c41c7f89398b90bfe3a370e22d118cd
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-04-22 14:07:55 -0700

    Atualização do núcleo da linguagem para a versão 0.17.0.

--- fontes/delegua.ts
+++ fontes/delegua.ts
@@ -275,9 +275,9 @@ export class Delegua implements DeleguaInterface {
         try {
             const manifesto = caminho.resolve(__dirname, 'package.json');
 
-            return JSON.parse(sistemaArquivos.readFileSync(manifesto, { encoding: 'utf8' })).version || '0.12';
+            return JSON.parse(sistemaArquivos.readFileSync(manifesto, { encoding: 'utf8' })).version || '0.17';
         } catch (error: any) {
-            return '0.16 (desenvolvimento)';
+            return '0.17 (desenvolvimento)';
         }
     }
 
