2beb32e1b60baf762b96c56b9361ef4e132380f2
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-08-18 17:35:11 -0700

    Um teste unitário, para o diretório não ficar vazio.

---
+++ testes/nucleo-execucao.test.ts
@@ -0,0 +1,20 @@
+import * as sistemaArquivos from 'fs';
+import * as caminho from 'path';
+import {expect, jest, test} from '@jest/globals';
+
+import { NucleoExecucao } from '../fontes/nucleo-execucao';
+
+jest.mock('sistemaArquivos');
+jest.mock('path');
+
+describe('Núcleo de execução', () => {
+    it('`executarCodigoComoArgumento`, trivial', async () => {
+        let retornoSaida: string = '';
+        const funcaoDeRetorno = (saida: string) => retornoSaida = saida;
+        const nucleoExecucao = new NucleoExecucao(funcaoDeRetorno);
+        nucleoExecucao.configurarDialeto();
+        await nucleoExecucao.executarCodigoComoArgumento('escreva("Olá mundo!")');
+
+        expect(retornoSaida).toBe('Olá mundo!');
+    });
+});
