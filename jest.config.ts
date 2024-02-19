abe5dc767e0444fe95b17d5369d9cd1f4053adfb
Leonel Sanches da Silva <leonel@designliquido.com.br>
2024-02-18 18:57:43 -0800

    Orientação para testes com módulos linkados.

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,10 @@ export default async (): Promise<Config.InitialOptions> => {
         coverageReporters: ['json-summary', 'lcov', 'text', 'text-summary'],
         detectOpenHandles: true,
         moduleNameMapper: {
+            // Se for utilizar módulos linkados, comentar a linha abaixo:
             '@designliquido/delegua/(.*)': '<rootDir>/node_modules/@designliquido/delegua/$1'
+            // E descomentar a linha abaixo:
+            // '@designliquido/delegua/(.*)': '<rootDir>/node_modules/@designliquido/delegua/dist/$1'
         },
         // TODO: Até então não conseguimos fazer funcionar.
         // Mantido aqui caso seja útil no futuro.
