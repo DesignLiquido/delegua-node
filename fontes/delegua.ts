3ef217a2055d16719cb5e75f554e6316d2dbe161
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-04-08 15:08:27 -0700

    Atualização do número da versão padrão.

--- fontes/delegua.ts
+++ fontes/delegua.ts
@@ -38,9 +38,9 @@ export class Delegua implements DeleguaInterface {
         try {
             const manifesto = caminho.resolve(process.cwd(), 'package.json');
 
-            return JSON.parse(sistemaArquivos.readFileSync(manifesto, { encoding: 'utf8' })).version || '0.26';
+            return JSON.parse(sistemaArquivos.readFileSync(manifesto, { encoding: 'utf8' })).version || '0.33';
         } catch (error: any) {
-            return '0.32 (desenvolvimento)';
+            return '0.33 (desenvolvimento)';
         }
     }
 
