48ecb15300d630fcaf53674865f2a05e1c920c05
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-05-25 10:35:17 -0700

    Atualização do dialeto Portugol Studio para a versão 0.2.9.

--- fontes/delegua.ts
+++ fontes/delegua.ts
@@ -40,7 +40,7 @@ export class Delegua implements DeleguaInterface {
 
             return JSON.parse(sistemaArquivos.readFileSync(manifesto, { encoding: 'utf8' })).version || '0.33';
         } catch (error: any) {
-            return '0.33 (desenvolvimento)';
+            return '0.35 (desenvolvimento)';
         }
     }
 
