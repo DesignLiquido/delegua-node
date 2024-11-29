bb167e12f8e6e53c9ab7e3a72248bdb5c13bb98f
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-11-29 15:30:04 -0800

    Atualização de números de versão no modo LAIR.

--- fontes/delegua.ts
+++ fontes/delegua.ts
@@ -38,9 +38,9 @@ export class Delegua implements DeleguaInterface {
         try {
             const manifesto = caminho.resolve(process.cwd(), 'package.json');
 
-            return JSON.parse(sistemaArquivos.readFileSync(manifesto, { encoding: 'utf8' })).version || '0.33';
+            return JSON.parse(sistemaArquivos.readFileSync(manifesto, { encoding: 'utf8' })).version || '0.37';
         } catch (error: any) {
-            return '0.35 (desenvolvimento)';
+            return '0.37 (desenvolvimento)';
         }
     }
 
