91ecdd59a454d97a98c5cb2ecfd929d3b06f791c
Leonel Sanches da Silva <leonel@designliquido.com.br>
2025-08-21 10:41:14 -0700

    Atualização periódica de pacotes.

--- fontes/delegua.ts
+++ fontes/delegua.ts
@@ -40,7 +40,7 @@ export class Delegua implements DeleguaInterface {
 
             return JSON.parse(sistemaArquivos.readFileSync(manifesto, { encoding: 'utf8' })).version || '0.41';
         } catch (error: any) {
-            return '0.45 (núcleo, desenvolvimento)';
+            return '0.50 (núcleo, desenvolvimento)';
         }
     }
 
