74dad23e735eefe7fc60df9303f2386bdb70e101
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2025-11-14 17:51:49 -0800

    Esboços de interpretadores de Pituguês com suporte a depuração.

---
+++ fontes/interpretador/dialetos/interpretador-pitugues-com-depuracao-importacao.ts
@@ -0,0 +1,95 @@
+import { InterpretadorComDepuracao } from "@designliquido/delegua/interpretador/depuracao";
+import { ResultadoParcialInterpretadorInterface, SimboloInterface } from '@designliquido/delegua/interfaces';
+import { Const, Declaracao, FuncaoDeclaracao } from "@designliquido/delegua/declaracoes";
+import { DescritorTipoClasse } from "@designliquido/delegua/interpretador/estruturas";
+import { inferirTipoVariavel } from "@designliquido/delegua/inferenciador";
+import { AcessoMetodoOuPropriedade } from "@designliquido/delegua/construtos";
+
+import { ImportadorInterface } from "../../interfaces";
+import { ModuloDeclaracoes } from '../../declaracoes';
+import { InterpretadorComImportacaoInterface } from '../../interfaces/interpretador-com-importacao-interface';
+import { ImportarBiblioteca } from '../../construtos';
+
+import * as comum from '../comum';
+
+export class InterpretadorPituguesComDepuracaoImportacao 
+    extends InterpretadorComDepuracao 
+    implements InterpretadorComImportacaoInterface
+{
+    importador: ImportadorInterface<SimboloInterface>;
+
+    constructor(
+        importador: ImportadorInterface<SimboloInterface>,
+        diretorioBase: string, 
+        funcaoDeRetorno: Function, 
+        funcaoDeRetornoMesmaLinha: Function) 
+    {
+        super(diretorioBase, funcaoDeRetorno, funcaoDeRetornoMesmaLinha);
+        this.importador = importador;
+    }
+
+    /**
+     * Efetivamente executa uma declaração.
+     * Reintroduzido aqui porque o método `executar()` não está obedecendo à herança como deveria.
+     * 
+     * TODO: Remover após resolver o problema com a herança. 
+     *
+     * @param declaracao A declaração a ser executada.
+     * @returns O resultado parcial da execução, normalmente usado por
+     *          ferramentas externas.
+     */
+    override async executar(declaracao: Declaracao): Promise<ResultadoParcialInterpretadorInterface> {
+        const resultado: any = await declaracao.aceitar(this);
+
+        // Alguns casos não possuem retorno, como declarações `se`, `enquanto`, etc.,
+        // que não satisfazem suas respectivas condições.
+        if (resultado === null || resultado === undefined) {
+            return null;
+        }
+
+        // Se o retorno já possui um `valorRetornado`, apenas retorna o resultado.
+        if (resultado.hasOwnProperty('valorRetornado')) {
+            return resultado;
+        }
+
+        let tipoResultado = resultado.tipo;
+        switch (resultado.constructor) {
+            case DescritorTipoClasse:
+                tipoResultado = resultado.simboloOriginal.lexema;
+                break;
+            default:
+                if (!tipoResultado) {
+                    tipoResultado = inferirTipoVariavel(resultado);
+                }
+                break;
+        }
+
+        return {
+            hashArquivo: declaracao.hashArquivo,
+            linha: declaracao.linha,
+            valorRetornado: resultado,
+            tipo: tipoResultado,
+        } as ResultadoParcialInterpretadorInterface;
+    }
+
+    async visitarConstrutoImportarBiblioteca(importarBiblioteca: ImportarBiblioteca) {
+        return comum.visitarConstrutoImportarBiblioteca(this, importarBiblioteca);
+    }
+
+    override visitarDeclaracaoConst(declaracao: Const): Promise<any> {
+        return comum.visitarDeclaracaoConst(this, declaracao);
+    }
+
+    override visitarDeclaracaoDefinicaoFuncao(funcaoDeclaracao: FuncaoDeclaracao) {
+        return comum.visitarDeclaracaoDefinicaoFuncao(this, funcaoDeclaracao);
+    }
+
+    async visitarDeclaracaoModuloDeclaracoes(declaracao: ModuloDeclaracoes) {
+        return comum.visitarDeclaracaoModuloDeclaracoes(this, declaracao);
+    }
+
+    // TODO: Passar lógica para o núcleo e apagar.
+    override async visitarExpressaoAcessoMetodoOuPropriedade(expressao: AcessoMetodoOuPropriedade) {
+        return comum.visitarExpressaoAcessoMetodoOuPropriedade(this, expressao);
+    }
+}
---
+++ fontes/interpretador/dialetos/interpretador-pitugues-com-importacao.ts
@@ -0,0 +1,70 @@
+import { Const, Declaracao, FuncaoDeclaracao } from '@designliquido/delegua/declaracoes';
+import { SimboloInterface } from '@designliquido/delegua/interfaces';
+import { InterpretadorPitugues } from '@designliquido/delegua/interpretador/dialetos/pitugues';
+import { AcessoMetodoOuPropriedade } from '@designliquido/delegua/construtos';
+
+import tipoDeDadosPrimitivos from '@designliquido/delegua/tipos-de-dados/primitivos';
+
+import { InterpretadorComImportacaoInterface } from '../../interfaces/interpretador-com-importacao-interface';
+import { ImportadorInterface } from '../../interfaces/importador-interface';
+import { ModuloDeclaracoes } from '../../declaracoes';
+import { ImportarBiblioteca } from '../../construtos';
+
+import * as comum from '../comum';
+
+/**
+ * O Interpretador visita todos os elementos complexos gerados pelo avaliador sintático (_parser_),
+ * e de fato executa a lógica de programação descrita no código.
+ */
+export class InterpretadorPituguesComImportacao 
+    extends InterpretadorPitugues 
+    implements InterpretadorComImportacaoInterface
+{
+    importador: ImportadorInterface<SimboloInterface>;
+
+    constructor(
+        importador: ImportadorInterface<SimboloInterface>,
+        diretorioBase: string,
+        performance = false,
+        funcaoDeRetorno: Function = null,
+        funcaoDeRetornoMesmaLinha: Function = null
+    ) {
+        super(diretorioBase, performance, funcaoDeRetorno, funcaoDeRetornoMesmaLinha);
+        this.importador = importador;
+    }
+
+    override async executar(declaracao: Declaracao, mostrarResultado = false): Promise<any> {
+        const resultado = await super.executar(declaracao);
+
+        if (mostrarResultado) {
+            this.funcaoDeRetorno(this.paraTexto(resultado));
+        }
+
+        if (resultado || typeof resultado === tipoDeDadosPrimitivos.BOOLEANO) {
+            this.resultadoInterpretador.push(resultado);
+        }
+
+        return resultado;
+    }
+
+    async visitarConstrutoImportarBiblioteca(importarBiblioteca: ImportarBiblioteca) {
+        return comum.visitarConstrutoImportarBiblioteca(this, importarBiblioteca);
+    }
+
+    override visitarDeclaracaoConst(declaracao: Const): Promise<any> {
+        return comum.visitarDeclaracaoConst(this, declaracao);
+    }
+
+    override visitarDeclaracaoDefinicaoFuncao(funcaoDeclaracao: FuncaoDeclaracao) {
+        return comum.visitarDeclaracaoDefinicaoFuncao(this, funcaoDeclaracao);
+    }
+
+    async visitarDeclaracaoModuloDeclaracoes(declaracao: ModuloDeclaracoes) {
+        return comum.visitarDeclaracaoModuloDeclaracoes(this, declaracao);
+    }
+
+    // TODO: Passar lógica para o núcleo e apagar.
+    override async visitarExpressaoAcessoMetodoOuPropriedade(expressao: AcessoMetodoOuPropriedade) {
+        return comum.visitarExpressaoAcessoMetodoOuPropriedade(this, expressao);
+    }
+}
